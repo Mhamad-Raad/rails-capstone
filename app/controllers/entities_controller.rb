c00f21e98a1fa8da94e0e1f367b1c0594d442f0e
joker69216 <hamaraad883@gmail.com>
2023-02-16 14:41:12 +0300

    add new page and functionality

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -4,4 +4,31 @@ class EntitiesController < ApplicationController
     @entities = @group.entities.order(created_at: :desc)
     @total_amount = @entities.sum(:amount)
   end
+
+  def new
+    @group = Group.find(params[:group_id])
+    @entity = @group.entities.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    p entity_params[:name]
+    p 'asfd;jihabsfljasn;fjna;jsa;nfa;jnfa;kjnsf;na;jsfa;fbs;adjblshfbludsb'
+    @entity = @group.entities.new(name: entity_params[:name], amount: entity_params[:amount], author_id: current_user.id)
+
+    if @entity.save!
+      @membership = Membership.create(group_id: entity_params[:group_id], entity_id: @entity.id)
+      redirect_to group_entities_path(@group), notice: "Transaction was successfully created."
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def entity_params
+    params.require(:entity).permit(:name, :amount, :group_id)
+  end
+
 end
