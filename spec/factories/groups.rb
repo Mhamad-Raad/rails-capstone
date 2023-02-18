7e167aa64264e74448bb3bac22e017cf7ef2961d
joker69216 <hamaraad12@hotmail.com>
2023-02-19 02:19:35 +0300

    add tests for the entities controller

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -2,7 +2,7 @@
 FactoryBot.define do
   factory :group do
     name { Faker::Lorem.word }
-    icon { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'files', 'test_image.png'), 'image/png') }
+    icon { Faker::Lorem.word }
     user
   end
 end
