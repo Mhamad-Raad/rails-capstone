36d9b1c99f9691b74b95f5280b4d331584dbdb00
joker69216 <hamaraad883@gmail.com>
2023-02-19 02:20:38 +0300

    fix rubocop linter

--- spec/controllers/entities_controller_spec.rb
+++ spec/controllers/entities_controller_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe EntitiesController, type: :controller do
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'returns http success' do
       group = FactoryBot.create(:group)
       get :index, params: { group_id: group.id }
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
+  describe 'GET #new' do
+    it 'returns http success' do
       group = FactoryBot.create(:group)
       get :new, params: { group_id: group.id }
       expect(response).to have_http_status(:success)
